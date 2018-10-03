6963c0d3bc3fe62efefe230e58fe10e62f44be86
selenium67 <seleniumjune18@gmail.com>
2018-10-03 15:36:51 +0530

    First Commit

---
+++ com.Demo.Project/src/test/java/com/Demo/Project/testClass/TestNGExecutionOrder.java
@@ -0,0 +1,68 @@
+package com.Demo.Project.testClass;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class TestNGExecutionOrder {
+	@Test
+	public void f() {
+		
+		System.out.println(" @Test This Method 5th ");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		
+		System.out.println(" @BeforeMethod This Method 4th ");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		
+		System.out.println(" @AfterMethod This Method 6th ");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		
+		System.out.println(" @BeforeClass This Method 3rd ");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		
+		System.out.println(" @AfterClass This Method 7th ");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		
+		System.out.println(" @BeforeTest This Method 2st ");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		
+		System.out.println(" @BeforeTest This Method 8th ");
+	}
+
+	@BeforeSuite
+	public void beforeSuite() {
+		
+		System.out.println(" @BeforeSuite This Method 1st ");
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		
+		System.out.println(" @BeforeSuite This Method 9th ");
+	}
+
+}
